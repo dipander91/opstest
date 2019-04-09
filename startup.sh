4aca52d2cd5d9ee14f68426c133dfdc127944ffd
dipander91 <35428032+dipander91@users.noreply.github.com>
2019-04-09 09:50:30 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-nohup java -jar /var/www/html/suchapp-0.0.1-SNAPSHOT.jar &
-/dev/null 2> /dev/null < /dev/null &
-exit 0
+nohup java -jar /var/www/html/suchapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
