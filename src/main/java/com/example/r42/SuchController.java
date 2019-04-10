27ba787575bb8ca5a9bf6be19fd7ea0b4ec84487
dipander91 <35428032+dipander91@users.noreply.github.com>
2019-04-10 17:04:49 +0200

    added code to fetch time and availibility zone

--- src/main/java/com/example/r42/SuchController.java
+++ src/main/java/com/example/r42/SuchController.java
@@ -3,6 +3,8 @@ package com.example.r42;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Timestamp;
+import com.amazonaws.util.EC2MetadataUtils;
 
 @RestController
 public class SuchController {
@@ -11,6 +13,6 @@ public class SuchController {
 
 	@RequestMapping("/hello")
 	public String suchHello(){
-		return "hello " + suchName;
+		return "hello " + suchName + " From " +  EC2MetadataUtils.getAvailabilityZone() + new Timestamp(System.currentTimeMillis());
 	}
 }
