a98c0b0a98a867ef58ae96e4d0fe57086556762f
dipander91 <35428032+dipander91@users.noreply.github.com>
2019-04-10 22:39:47 +0530

    Update SuchController.java

--- src/main/java/com/example/r42/SuchController.java
+++ src/main/java/com/example/r42/SuchController.java
@@ -3,8 +3,7 @@ package com.example.r42;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.sql.Timestamp;
-import com.amazonaws.util.EC2MetadataUtils;
+
 
 @RestController
 public class SuchController {
@@ -13,6 +12,6 @@ public class SuchController {
 
 	@RequestMapping("/hello")
 	public String suchHello(){
-		return "hello " + suchName + " From " +  EC2MetadataUtils.getAvailabilityZone() + new Timestamp(System.currentTimeMillis());
+		return "hello " + suchName;
 	}
 }
