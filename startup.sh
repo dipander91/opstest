1bd7c0522acff9fdc9857c26b727cd5e7eaac50e
dipander91 <35428032+dipander91@users.noreply.github.com>
2019-04-11 11:32:59 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kill $(cat ./pid.file)
-nohup java -jar /var/www/html/suchapp-0.0.1-SNAPSHOT.jar & echo $! > ./pid.file > /dev/null 2> /dev/null < /dev/null & 
+nohup java -jar /var/www/html/suchapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & 
